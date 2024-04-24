27f425d781d117a7b56cbf603475d955c43b8c6b
Psuedoo <forthewin720@gmail.com>
2024-04-23 22:38:00 -0400

    change page spacing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { TaskTable } from "./task";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-3xl">Welcome to Prioritea!</h1>
       <TaskTable />
     </main>
